45c4ca86e70a6f8c0c2a6e6b4a496cc98263262f
Tom Victor <vjtomvictor@icloud.com>
2024-02-23 18:23:06 +0100

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,9 @@ export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/">
+        <Link to={`/home`} class="navbar-brand">
           Skill Map
-        </a>
+        </Link>
         <button
           class="navbar-toggler"
           type="button"
